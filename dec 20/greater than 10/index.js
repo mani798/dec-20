d14e63dd1656c71e424f7e5a96080aed5279e459
mani798 <64831331+mani798@users.noreply.github.com>
2022-01-02 14:31:57 +0530

    Add files via upload

---
+++ dec 20/greater than 10/index.js
@@ -0,0 +1,7 @@
+var ab=80;
+if(ab>10){
+console.log("it is greater than 10")
+}
+else {
+console.log("it is lessthan than 10")
+}
---
+++ dec 20/print greatest number/index.js
@@ -0,0 +1,14 @@
+
+var a =30;
+var b =20;
+var c =52;
+
+if(a>=b && a>=c){
+console.log(a);
+}
+else if(b>=a && b>=c){
+console.log(b);
+}
+else{
+console.log(c);
+}
